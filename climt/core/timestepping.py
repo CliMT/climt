f7f7793279a0c49401b5675d36616a8134c42045
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2016-11-26 16:55:30 -0800

    Added base class for TimeStepper and two skeleton TimeSteppers

---
+++ climt/core/timestepping.py
@@ -0,0 +1,13 @@
+
+class TimeStepper(object):
+
+    def __init__(self, prognostic_list, diagnostic_list=(), **kwargs):
+        pass
+
+
+class AdamsBashforth(TimeStepper):
+    pass
+
+
+class Leapfrog(TimeStepper):
+    pass
