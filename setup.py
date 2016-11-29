e225449e9d39ab4c032a922137bd2d59f2f006a4
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2016-11-29 09:45:26 -0800

    Added requirements to setup.py
    Added numpy>=1.11 to package requirements, and pytest and mock to test_requirements.
    Test requirements are already installed by tox from requirements_dev.

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'numpy>=1.11',
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'pytest>=2.9.2',
+    'mock>=2.0.0',
 ]
 
 setup(
