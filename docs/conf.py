51bfc8d9e34bfb7e8f4d5c24ba52739ab871ad4c
Joy M. Monteiro <joy.merwin@gmail.com>
2017-03-30 21:57:28 +0200

    trying some more tricks for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_style = 'default'
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
