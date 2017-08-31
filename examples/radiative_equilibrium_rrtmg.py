1debec2fdaebf08d911f8f75a2071ad9bdd08dcd
Joy M. Monteiro <joy.merwin@gmail.com>
2017-08-31 10:11:55 +0200

    was broken. Also cleaned up visualisation

--- examples/radiative_equilibrium_rrtmg.py
+++ examples/radiative_equilibrium_rrtmg.py
@@ -1,32 +1,55 @@
 from sympl import (
     AdamsBashforth, PlotFunctionMonitor)
-from climt import RRTMGShortwave, get_default_state
+from climt import RRTMGShortwave, RRTMGLongwave, get_default_state
 import numpy as np
 from datetime import timedelta
 
 
 def plot_function(fig, state):
-    ax = fig.add_subplot(1, 1, 1)
+    ax = fig.add_subplot(1, 2, 1)
     ax.plot(
         state['shortwave_heating_rate'].values.flatten(),
         state['air_pressure'].values.flatten(), '-o')
     ax.axes.invert_yaxis()
-    # print(state['air_temperature'].values.flatten())
-    # print(state['air_pressure'].values.flatten())
-    # ax.set_yscale('log')
+    ax.plot(
+        state['longwave_heating_rate'].values.flatten(),
+        state['air_pressure'].values.flatten(), '-o')
+    ax.axes.invert_yaxis()
+    ax.set_title('Heating Rates')
+    ax.grid()
+    ax.set_xlabel('K/day')
+    ax.set_ylabel('millibar')
+
+    ax.set_yscale('log')
     ax.set_ylim(1e5, 1.)
+    ax = fig.add_subplot(1, 2, 2)
+    ax.plot(
+        state['air_temperature'].values.flatten(),
+        state['air_pressure'].values.flatten(), '-o')
+    ax.axes.invert_yaxis()
+
+    ax.set_yscale('log')
+    ax.set_ylim(1e5, 1.)
+    ax.set_title('Temperature')
+    ax.grid()
+    ax.set_xlabel('K')
+    ax.set_yticklabels([])
 
 
 monitor = PlotFunctionMonitor(plot_function)
-radiation = RRTMGShortwave()
-time_stepper = AdamsBashforth([radiation])
-timestep = timedelta(hours=4)
+rad_sw = RRTMGShortwave()
+rad_lw = RRTMGLongwave()
+time_stepper = AdamsBashforth([rad_sw, rad_lw])
+timestep = timedelta(hours=1)
 
-z_information = {'label': 'vertical_level',
+mid_levels = {'label': 'mid_level',
                  'values': np.arange(60),
                  'units': ''}
 
-state = get_default_state([radiation], z=z_information)
+int_levels = {'label': 'interface_level',
+                 'values': np.arange(61),
+                 'units': ''}
+state = get_default_state([rad_sw, rad_lw], mid_levels=mid_levels, interface_levels=int_levels)
 
 tp_profiles = np.load('thermodynamic_profiles.npz')
 mol_profiles = np.load('molecule_profiles.npz')
@@ -35,15 +58,16 @@ state['air_pressure'].values[0, 0, :] = tp_profiles['air_pressure']
 state['air_temperature'].values[0, 0, :] = tp_profiles['air_temperature']
 state['air_pressure_on_interface_levels'].values[0, 0, :] = tp_profiles['interface_pressures']
 
-# state['specific_humidity'].values[0, 0, :] = mol_profiles['specific_humidity']
+state['specific_humidity'].values[0, 0, :] = mol_profiles['specific_humidity']*1e-3
 state['mole_fraction_of_carbon_dioxide_in_air'].values[0, 0, :] = mol_profiles['carbon_dioxide']
 state['mole_fraction_of_ozone_in_air'].values[0, 0, :] = mol_profiles['ozone']
 
-for i in range(8000):
+
+for i in range(1000):
 
     # print(i)
-    diagnostics, new_state = time_stepper.__call__(state, timestep)
+    diagnostics, new_state = time_stepper(state, timestep)
     state.update(diagnostics)
-    if i % 200 == 0:
+    if i % 2 == 0:
         monitor.store(state)
     state = new_state
