94d21a2774d8f2b1a07e34f88632a0298f49aa59
Joy M. Monteiro <joy.merwin@gmail.com>
2018-01-16 02:38:48 +0100

    moved SimplePhysics to new API

--- climt/_components/simple_physics/component.py
+++ climt/_components/simple_physics/component.py
@@ -1,9 +1,8 @@
 from sympl import DataArray
 from sympl import get_numpy_array
 from sympl import combine_dimensions
-from ..._core import ClimtImplicit, ClimtImplicitPrognostic, get_constant
+from ..._core import ClimtImplicit, get_constant
 import numpy as np
-import copy
 try:
     from . import _simple_physics as phys
 except ImportError:
@@ -130,7 +129,6 @@ class SimplePhysics(ClimtImplicit):
         self._surface_flux = surface_fluxes
         self._use_ext_ts = use_external_surface_temperature
         self._use_ext_qsurf = use_external_surface_specific_humidity
-        self._return_tend = False
 
         phys.init_simple_physics(self._cyclone, self._lsc, self._pbl,
                                  self._surface_flux, self._use_ext_ts,
@@ -230,14 +228,6 @@ class SimplePhysics(ClimtImplicit):
 
         latent_heat_flux[latent_heat_flux < 0] = 0
 
-        if self._return_tend:
-            t_out = (t_out - T)/timestep.total_seconds()
-            u_out = (u_out - U)/timestep.total_seconds()
-            v_out = (v_out - V)/timestep.total_seconds()
-            q_out = (q_out - q)/timestep.total_seconds()
-            return (t_out, u_out, v_out, q_out, precip_out,
-                    sensible_heat_flux, latent_heat_flux)
-
         new_state = {
             'eastward_wind': DataArray(
                 u_out, dims=dims_mid, attrs=state['eastward_wind'].attrs
@@ -260,63 +250,3 @@ class SimplePhysics(ClimtImplicit):
         diagnostics['surface_upward_latent_heat_flux'].values[:] = latent_heat_flux
 
         return diagnostics, new_state
-
-    def get_prognostic_version(self, model_time_step):
-        """
-        Get a component which returns tendencies instead of new model state.
-
-        This is mainly to allow SimplePhysics to be used with the spectral
-        dynamical core.
-
-        Args:
-            model_time_step (timedelta):
-                The model time step used.
-
-        Returns:
-            SimplePhysicsWithTendencies(ClimtImplicitPrognostic):
-                Version of SimplePhysics which returns tendencies.
-
-        """
-
-        new_self = copy.deepcopy(self)
-        new_self._return_tend = True
-        return SimplePhysicsWithTendencies(new_self, model_time_step)
-
-
-class SimplePhysicsWithTendencies(ClimtImplicitPrognostic):
-    """
-    Version of simple physics that returns tendencies
-    """
-
-    def __init__(self, simple_physics, timestep):
-
-        self._implicit_simple_physics = simple_physics
-        self.current_time_step = timestep
-
-        self._climt_inputs = simple_physics._climt_inputs
-        self._climt_diagnostics = simple_physics._climt_diagnostics
-        self._climt_tendencies = simple_physics._climt_outputs
-        self._climt_tendencies['air_temperature'] = 'degK s^-1'
-        self._climt_tendencies['eastward_wind'] = 'm s^-2'
-        self._climt_tendencies['northward_wind'] = 'm s^-2'
-        self._climt_tendencies['specific_humidity'] = 'g g^-1 s^-1'
-
-    def __call__(self, state):
-        (t_out, u_out, v_out, q_out, precip_out,
-         sensible_heat_flux, latent_heat_flux) =\
-            self._implicit_simple_physics(state,
-                                          self.current_time_step)
-
-        tendencies = self.create_state_dict_for('_climt_tendencies', state)
-        tendencies['air_temperature'].values = t_out
-        tendencies['eastward_wind'].values = u_out
-        tendencies['northward_wind'].values = v_out
-        tendencies['specific_humidity'].values = q_out
-
-        diagnostics = self.create_state_dict_for('_climt_diagnostics', state)
-
-        diagnostics['stratiform_precipitation_rate'].values = precip_out
-        diagnostics['surface_upward_sensible_heat_flux'].values = sensible_heat_flux
-        diagnostics['surface_upward_latent_heat_flux'].values = latent_heat_flux
-
-        return tendencies, diagnostics
