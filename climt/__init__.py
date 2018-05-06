5d4073f24d01274bcb069f31e0b34a443adaaa75
Joy M. Monteiro <joy.merwin@gmail.com>
2018-05-06 20:56:18 +0200

    Bump version: 0.14.2 → 0.14.3

--- climt/__init__.py
+++ climt/__init__.py
@@ -33,4 +33,4 @@ __all__ = (
     EmanuelConvection, SlabSurface, GFSDynamicalCore, DcmipInitialConditions, IceSheet,
     Instellation)
 
-__version__ = '0.14.2'
+__version__ = '0.14.3'
--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ else:
 
 setup(
     name='climt',
-    version='0.14.2',
+    version='0.14.3',
     description='CliMT is a Toolkit for building Earth system models in Python.',
     long_description=readme + '\n\n' + history,
     author="Rodrigo Caballero",
