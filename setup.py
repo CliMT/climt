283205682f1e5c484db6b40bb203de4f625e67a7
Joy Monteiro <joy.merwin@gmail.com>
2018-05-06 23:18:05 +0200

    Bump version: 0.14.4 → 0.14.5

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ else:
 
 setup(
     name='climt',
-    version='0.14.4',
+    version='0.14.5',
     description='CliMT is a Toolkit for building Earth system models in Python.',
     long_description=readme + '\n\n' + history,
     author="Rodrigo Caballero",
