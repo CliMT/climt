dfceca39fad3f86ae2c6574eb8206e547cc43864
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2018-06-08 17:03:48 -0700

    Updated simple_physics to new api, supporting wildcard horizontal dims

--- climt/__init__.py
+++ climt/__init__.py
@@ -4,11 +4,8 @@ import sympl
 
 from ._core import (
     get_default_state, mass_to_volume_mixing_ratio,
-    get_interface_values, climt_quantity_descriptions, numpy_version_of,
+    get_interface_values, numpy_version_of,
     bolton_q_sat, bolton_dqsat_dT, calculate_q_sat,
-    ClimtImplicitPrognostic, ClimtPrognostic, ClimtImplicit, ClimtDiagnostic,
-    ClimtSpectralDynamicalCore,
-    set_constant, get_constant, reset_constants,
     list_available_constants, set_constants_from_dict)
 
 from ._components import (
@@ -18,16 +15,11 @@ from ._components import (
     DcmipInitialConditions, IceSheet, Instellation)
 
 
-sympl.add_direction_names(z=['interface_levels', 'mid_levels'])
-
 __all__ = (
     get_default_state, mass_to_volume_mixing_ratio, numpy_version_of,
-    get_interface_values, get_constant, set_constant,
-    reset_constants, list_available_constants,
+    get_interface_values, list_available_constants,
     set_constants_from_dict,
-    bolton_q_sat, bolton_dqsat_dT, climt_quantity_descriptions, calculate_q_sat,
-    ClimtImplicitPrognostic, ClimtPrognostic, ClimtImplicit, ClimtDiagnostic,
-    ClimtSpectralDynamicalCore,
+    bolton_q_sat, bolton_dqsat_dT, calculate_q_sat,
     Frierson06LongwaveOpticalDepth, GrayLongwaveRadiation, HeldSuarez,
     GridScaleCondensation, BergerSolarInsolation, SimplePhysics, RRTMGLongwave, RRTMGShortwave,
     EmanuelConvection, SlabSurface, GFSDynamicalCore, DcmipInitialConditions, IceSheet,
--- climt/_core/__init__.py
+++ climt/_core/__init__.py
@@ -4,20 +4,14 @@ from .util import (
     mass_to_volume_mixing_ratio,
     get_interface_values, numpy_version_of,
     bolton_q_sat, bolton_dqsat_dT, calculate_q_sat)
-from .initialization import get_default_state, climt_quantity_descriptions
-from .climt_components import (
-    ClimtImplicitPrognostic, ClimtPrognostic, ClimtImplicit, ClimtDiagnostic,
-    ClimtSpectralDynamicalCore
-)
+from .initialization import get_default_state
 from .constants import (list_available_constants,
                         set_constants_from_dict)
 
 __all__ = (
     mass_to_volume_mixing_ratio, get_default_state,
-    get_interface_values, climt_quantity_descriptions,
+    get_interface_values,
     bolton_q_sat, bolton_dqsat_dT, calculate_q_sat, numpy_version_of,
-    ClimtImplicitPrognostic, ClimtPrognostic, ClimtImplicit,
-    ClimtDiagnostic, ClimtSpectralDynamicalCore,
     set_constant, get_constant, reset_constants,
     list_available_constants, set_constants_from_dict
 )
