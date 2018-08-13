9cd867c0cf5af558f67a47c8bba17df4944f8245
Jeremy McGibbon <mcgibbon@uw.edu>
2018-08-13 11:58:07 -0700

    removed unnecessary dims definition

--- climt/_components/radiation.py
+++ climt/_components/radiation.py
@@ -50,9 +50,7 @@ class GrayLongwaveRadiation(TendencyComponent):
     }
 
     tendency_properties = {
-        'air_temperature': {
-            'dims': ['mid_levels', '*'],
-            'units': 'degK s^-1'}
+        'air_temperature': {'units': 'degK s^-1'},
     }
 
     def array_call(self, state):
