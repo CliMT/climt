38ab596a1cf0d8314548fd267f8473c0e717715c
Joy M. Monteiro <joy.merwin@gmail.com>
2018-08-18 21:56:27 +0200

    Revert "Bump version: 0.15.0 → 0.15.1"
    This reverts commit c6def10c83f249a052578911c7cef8282760e509.

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ else:
 
 setup(
     name='climt',
-    version='0.15.1',
+    version='0.15.0',
     description='CliMT is a Toolkit for building Earth system models in Python.',
     long_description=readme + '\n\n' + history,
     author="Rodrigo Caballero",
