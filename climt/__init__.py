fdfd38cd3f3b3d3bc1fa47b7880ee2ea8c37e2ee
Joy M. Monteiro <joy.merwin@gmail.com>
2019-07-29 09:24:08 +0200

    Bump version: 0.16.4 → 0.16.5

--- climt/__init__.py
+++ climt/__init__.py
@@ -28,4 +28,4 @@ __all__ = (
     EmanuelConvection, SlabSurface, GFSDynamicalCore, DcmipInitialConditions,
     IceSheet, Instellation, DryConvectiveAdjustment)
 
-__version__ = '0.16.4'
+__version__ = '0.16.5'
