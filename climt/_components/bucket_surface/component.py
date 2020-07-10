1088695303a69cc2f31121081be738dcc24c471b
Ai33L <abels2000@gmail.com>
2020-07-10 23:26:43 +0530

    Component redesigned to Stepper

--- climt/_components/bucket_surface/component.py
+++ climt/_components/bucket_surface/component.py
@@ -1,8 +1,7 @@
 from math import sqrt
-from sympl import TendencyComponent, initialize_numpy_arrays_with_properties
+from sympl import Stepper
 
-
-class BucketSurface(TendencyComponent):
+class BucketSurface(Stepper):
     """
     Calculates the surface energy and the hydrology balance.
 
@@ -75,17 +74,6 @@ class BucketSurface(TendencyComponent):
         },
     }
 
-    tendency_properties = {
-        'surface_temperature': {
-            'dims': ['*'],
-            'units': 'degK s^-1',
-        },
-        'lwe_thickness_of_soil_moisture_content':{
-            'dims': ['*'],
-            'units': 'm s^-1',
-        },
-    }
-
     diagnostic_properties = {
         'precipitation_rate': {
             'dims': ['*'],
@@ -105,6 +93,11 @@ class BucketSurface(TendencyComponent):
         },
     }
 
+    output_properties = {
+        'surface_temperature': {'units': 'degK'},
+        'lwe_thickness_of_soil_moisture_content':{'units': 'm'},
+    }
+
 
     def __init__(self, soil_moisture_max=0.15, g=0.75, specific_latent_heat_of_water=2260000,
                  bulk_coefficient=0.0011, **kwargs):
@@ -116,36 +109,31 @@ class BucketSurface(TendencyComponent):
         super(BucketSurface, self).__init__(**kwargs)
 
 
-    def array_call(self, raw_state):
-        diagnostics = initialize_numpy_arrays_with_properties(
-            self.diagnostic_properties, raw_state, self.input_properties
-        )
-        tendencies = initialize_numpy_arrays_with_properties(
-            self.tendency_properties, raw_state, self.input_properties
-        )
+    def array_call(self, state, timestep):
 
-        diagnostics['beta_factor'] = 0
 
-        wind_speed = sqrt(pow(raw_state['northward_wind'][0], 2) + \
-           pow(raw_state['eastward_wind'][0], 2))
+        state['beta_factor'] = 0
+
+        wind_speed = sqrt(pow(state['northward_wind'][0], 2) + \
+           pow(state['eastward_wind'][0], 2))
         evaporation_rate_max = self._c * wind_speed * \
-           (raw_state['surface_specific_humidity'] - raw_state['specific_humidity'][0])
+           (state['surface_specific_humidity'] - state['specific_humidity'][0])
 
 
-        precipitation_rate = raw_state['convective_precipitation_rate'] + \
-                             raw_state['stratiform_precipitation_rate']
+        precipitation_rate = state['convective_precipitation_rate'] + \
+                             state['stratiform_precipitation_rate']
 
 
-        soil_moisture = raw_state['lwe_thickness_of_soil_moisture_content']
+        soil_moisture = state['lwe_thickness_of_soil_moisture_content']
 
         soil_moisture_tendency = 0
 
         if soil_moisture >= self._g * self._smax:
-            diagnostics['beta_factor'] = 1
+            state['beta_factor'] = 1
         else:
-            diagnostics['beta_factor'] = soil_moisture/(self._g*self._smax)
+            state['beta_factor'] = soil_moisture/(self._g*self._smax)
 
-        evaporation_rate = diagnostics['beta_factor'] * evaporation_rate_max
+        evaporation_rate = state['beta_factor'] * evaporation_rate_max
 
         if soil_moisture < self._smax or precipitation_rate <= evaporation_rate:
             soil_moisture_tendency = precipitation_rate - evaporation_rate
@@ -153,27 +141,41 @@ class BucketSurface(TendencyComponent):
             soil_moisture_tendency = 0
 
 
-        diagnostics['surface_upward_latent_heat_flux'] = self._l * evaporation_rate
-        diagnostics['surface_upward_sensible_heat_flux'] = self._c * wind_speed * \
-        (raw_state['surface_temperature'] - raw_state['air_temperature'][0])
+        state['surface_upward_latent_heat_flux'] = self._l * evaporation_rate
+        state['surface_upward_sensible_heat_flux'] = self._c * wind_speed * \
+        (state['surface_temperature'] - state['air_temperature'][0])
 
         net_heat_flux = (
-            raw_state['downwelling_shortwave_flux_in_air'][:, 0] +
-            raw_state['downwelling_longwave_flux_in_air'][:, 0] -
-            raw_state['upwelling_shortwave_flux_in_air'][:, 0] -
-            raw_state['upwelling_longwave_flux_in_air'][:, 0] -
-            diagnostics['surface_upward_sensible_heat_flux'] -
-            diagnostics['surface_upward_latent_heat_flux']
+            state['downwelling_shortwave_flux_in_air'][:, 0] +
+            state['downwelling_longwave_flux_in_air'][:, 0] -
+            state['upwelling_shortwave_flux_in_air'][:, 0] -
+            state['upwelling_longwave_flux_in_air'][:, 0] -
+            state['surface_upward_sensible_heat_flux'] -
+            state['surface_upward_latent_heat_flux']
         )
 
 
-        mass_surface_slab = raw_state['surface_material_density'] * \
-            raw_state['soil_layer_thickness']
-        heat_capacity_surface = mass_surface_slab * raw_state['heat_capacity_of_soil']
+        mass_surface_slab = state['surface_material_density'] * \
+            state['soil_layer_thickness']
+        heat_capacity_surface = mass_surface_slab * state['heat_capacity_of_soil']
 
+        new_surface_temperature = state['surface_temperature'] + \
+            (net_heat_flux/heat_capacity_surface * timestep.total_seconds())
+        new_soil_moisture = state['lwe_thickness_of_soil_moisture_content'] + \
+            (soil_moisture_tendency * timestep.total_seconds())
 
-        tendencies = {'surface_temperature': net_heat_flux/heat_capacity_surface,
-                      'lwe_thickness_of_soil_moisture_content' : soil_moisture_tendency}
+        new_soil_moisture[new_soil_moisture > 0.15] = 0.15
 
+        new_state = {
+            'surface_temperature' : new_surface_temperature,
+            'lwe_thickness_of_soil_moisture_content' : new_soil_moisture,
+        }
 
-        return tendencies, diagnostics
+        diagnostics = {
+            'precipitation_rate': precipitation_rate,
+            'surface_upward_sensible_heat_flux': state['surface_upward_sensible_heat_flux'],
+            'surface_upward_latent_heat_flux': state['surface_upward_latent_heat_flux'],
+            'beta_factor': state['beta_factor'],
+        }
+
+        return diagnostics, new_state
