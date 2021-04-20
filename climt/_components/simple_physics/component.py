8ed5a8df325ef9a4ae976b32d2ec5e150ab6178b
JoyMonteiro <joy.merwin@gmail.com>
2021-04-20 22:28:14 +0530

    Update component.py

--- climt/_components/simple_physics/component.py
+++ climt/_components/simple_physics/component.py
@@ -1,6 +1,14 @@
 from ..._core import ensure_contiguous_state
 from sympl import Stepper, get_constant
-from . import _simple_physics as phys
+import logging
+try:
+    from . import _simple_physics as phys
+except ImportError as error:
+    logging.warning(
+        'Import failed. Simple Physics is likely not compiled and will not be'
+        'available.'
+    )
+    print(error)
 
 
 class SimplePhysics(Stepper):
