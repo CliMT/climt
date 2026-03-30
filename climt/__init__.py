4b82810330933e1eae4d479f45b0d6b122f6d0f8
Joy Monteiro <joy.merwin@gmail.com>
2026-03-30 23:56:08 +0530

    Bump version: 0.19.0 → 0.20.0

--- climt/__init__.py
+++ climt/__init__.py
@@ -67,4 +67,4 @@ __all__ = (
     BucketHydrology,
 )
 
-__version__ = "0.19.0"
+__version__ = "0.20.0"
--- setup.py
+++ setup.py
@@ -292,7 +292,7 @@ else:
 
 setup(
     name="climt",
-    version="0.19.0",
+    version="0.20.0",
     description="CliMT is a Toolkit for building Earth system models in Python.",
     long_description=readme + "\n\n" + history,
     author="Rodrigo Caballero",
