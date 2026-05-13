ead67130482a032290b9bd8bccc793f07f763602
Joy Monteiro <joy.merwin@gmail.com>
2026-05-13 11:02:12 +0530

    feat(core): export CondensibleParams, get_condensible_params, get_active_condensible

--- climt/_core/__init__.py
+++ climt/_core/__init__.py
@@ -2,8 +2,13 @@ from sympl import get_constant, reset_constants, set_constant
 
 from .atmospheric_properties import (
     get_constant_checked,
+    get_active_condensible,
     load_atmospheric_properties,
     reset_atmospheric_properties,
+)
+from .condensibles import (
+    CondensibleParams,
+    get_condensible_params,
 )
 from .exceptions import ConstantNotFoundError
 from .constants import list_available_constants, set_constants_from_dict
@@ -21,26 +26,28 @@ from .util import (
 )
 
 __all__ = (
-    mass_to_volume_mixing_ratio,
-    get_default_state,
-    get_grid,
-    get_interface_values,
-    get_constant_checked,
-    load_atmospheric_properties,
-    reset_atmospheric_properties,
-    ConstantNotFoundError,
-    bolton_q_sat,
-
-    bolton_dqsat_dT,
-    calculate_q_sat,
-    numpy_version_of,
-    set_constant,
-    get_constant,
-    reset_constants,
-    list_available_constants,
-    set_constants_from_dict,
-    ensure_contiguous_state,
-    UnytBackend,
-    UnytStateContainer,
-    UnytTimeDelta,
+    "mass_to_volume_mixing_ratio",
+    "get_default_state",
+    "get_grid",
+    "get_interface_values",
+    "get_constant_checked",
+    "get_active_condensible",
+    "CondensibleParams",
+    "get_condensible_params",
+    "load_atmospheric_properties",
+    "reset_atmospheric_properties",
+    "ConstantNotFoundError",
+    "bolton_q_sat",
+    "bolton_dqsat_dT",
+    "calculate_q_sat",
+    "numpy_version_of",
+    "set_constant",
+    "get_constant",
+    "reset_constants",
+    "list_available_constants",
+    "set_constants_from_dict",
+    "ensure_contiguous_state",
+    "UnytBackend",
+    "UnytStateContainer",
+    "UnytTimeDelta",
 )
